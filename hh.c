8eebc4bcb51833c18e8bcfbeee683b5ca5d8f27c
jiangyuwei <jyw_2011@126.com>
2011-04-17 18:39:40 +0800

    lianbiao

--- hh.c
+++ hh.c
@@ -1,38 +1,41 @@
 #include<stdio.h>
-
-void print_day(int day)
-{
-  switch(day)
-  {
-    case 1:
-      printf("monday\n");
-      break;
-    case 2:
-      printf("tuesday\n");
-      break;
-    case 3:
-      printf("wednesday\n");
-      break;
-    case 4:
-      printf("thursday\n");
-      break;
-    case 5:
-      printf("friday\n");
-      break;
-    case 6:
-      printf("saturday\n");
-      break;
-    case 7:
-      printf("sunday\n");
-      break;
-    default:
-     printf("illegal day number!\n");
-     break;
-
-  }
-}
+#include<string.h>
+int sting_to_number(char *s);
 int main(void)
 {
-  print_day(6);
-  return 0;
+  char str[100];
+  int number = 0;
+  printf("please input the number:\n");
+  scanf("%s",str);
+  
+  number  = sting_to_number(str);
+  if(number == -1)
+    {
+    printf("No number \n");
+    }
+  else
+    {
+    printf("number=%d\n",number);
+    }
+    return 0;
+}
+int sting_to_number(char *s)
+{
+    int number = 0;
+    if((*s>='0')&&(*s<='9'))
+    {
+       number = (*s-0x30);
+       s++;
+       while((*s>='0')&&(*s<='9'))
+       {
+           number *=10;
+           number +=(*s-0x30);
+           s++;
+       }
+    }
+    else
+    {
+      return -1;
+    }
+    return number;
 }
---
+++ hp.c
@@ -0,0 +1,152 @@
+#include<stdio.h>   
+#include<string.h>
+#include<stdlib.h>
+
+struct node
+{
+ int number;
+ char name[20];
+ struct node *next;
+};
+
+typedef struct node V_NODE;
+void print_link(V_NODE *head);
+V_NODE *add_link(V_NODE *head);
+V_NODE *delete_link(V_NODE *head);
+V_NODE *create_link(int n);
+
+int main(void)
+{   
+   V_NODE *head;
+  
+ 
+   int n = 0;
+   printf("Input a number:\n");
+   scanf("%d",&n);
+
+   head = create_link(n);
+   print_link(head);
+   head = add_link(head);
+   print_link(head);
+   head=delete_link(head);
+   print_link(head);
+
+   return 0;
+   
+}
+V_NODE *create_link(int n)
+{  
+  
+   V_NODE *p = NULL;
+   V_NODE *head = NULL;
+   int i=0;
+
+   p = head = malloc(sizeof(V_NODE));
+   if(p == NULL)
+   {
+       perror("malloc");
+       exit(0);
+
+   }
+   p->number = 1;     
+   strcpy(p->name,"a");
+   p->next = NULL;         
+   
+   for(i = 1; i < n; i++) 
+   {
+       p->next = malloc(sizeof(V_NODE));
+       if(p->next == NULL)
+       {
+            perror("malloc");
+            exit(0);
+       }
+       p->next->number = i+1;
+       strcpy(p->next->name,"a");
+       p->next->next = NULL;
+       p = p->next;
+      
+   }
+
+   return head;
+}
+
+V_NODE *add_link(V_NODE *head)
+{  
+   V_NODE *p = NULL;
+   V_NODE *ptr = head;
+              
+   p = malloc(sizeof(V_NODE));
+   if(p == NULL)
+   {
+      perror("malloc");
+      exit(0);
+   }
+   printf("input number:\n");
+   scanf("%d",&p->number);
+   printf("input name:\n");
+   scanf("%s",p->name);
+   p->next = NULL;
+   
+   if(ptr == NULL)
+   {
+       return p;
+   }
+   if(p->number<ptr->number)
+   {
+       p->next = ptr;
+       head = p;
+       return head;
+   }
+   while((ptr->next != NULL) && (p->number > ptr->next->number))
+   {
+      ptr = ptr->next;
+   }
+   p->next = ptr->next;
+   ptr->next = p;
+   return head;
+}
+V_NODE *delete_link(V_NODE *head)
+{ 
+  int num = 0;
+  V_NODE *p = head;
+  V_NODE *ptr = NULL;
+  if(head == NULL)
+  {
+    printf("empty link!\n");
+    return NULL;
+  }
+  printf("please input number of node to delete:\n");
+  scanf("%d",&num);
+  if(num == head->number)
+  {
+      head = head->next;
+      free(p);
+      return head;
+  }
+  while((p->next !=NULL) && (p->next->number != num))
+  {
+     p=p->next;
+  }
+  if(p->next == NULL)
+  {
+    printf("no number match!\n");
+  }
+  else
+  {
+   ptr = p->next;
+   p->next = p->next->next;
+   free(ptr);
+  }
+  return head;
+}
+
+void print_link(V_NODE *p)
+{
+   while(p != NULL)
+   {
+     printf("%d\t%s\n",p->number,p->name);
+     p = p->next;
+    // p->next->name = p->name + 1;
+   }
+   return ;
+}
